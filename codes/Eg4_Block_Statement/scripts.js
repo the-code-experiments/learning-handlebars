0baf8a73c02094d97f5cef18747fa777c4f98f04
hegdeashwin <ashwin.hegde3@gmail.com>
2016-06-29 02:03:57 +0530

    Adds eg for block statement

---
+++ codes/Eg4_Block_Statement/scripts.js
@@ -0,0 +1,24 @@
+'use strict';
+
+var containerTpl = document.querySelector('#container-template').innerHTML;
+var container = document.querySelector('#container');
+
+/**
+ * Data in the form for JavaScript object.
+ */
+var jsonData = {
+  'author': {
+    'firstname': 'Ashwin',
+    'lastname': 'Hegde'
+  }
+};
+
+/**
+ * Compile innerHTML using compile method.
+ */
+var template = Handlebars.compile(containerTpl);
+
+/**
+ * Passing object data to template.
+ */
+container.innerHTML = template({data: jsonData});
